855982e8b30b16874d685c3deb70e670b23b6347
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2025-12-25 12:14:30 +0600

    Add home manager config

--- flake.nix
+++ flake.nix
@@ -3,14 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+        home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations = {
       kvm-workstation = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+
+
         modules = [
           ./hosts/kvm-workstation/configuration.nix
+
+	  home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.abir = import ./home/abir/home.nix;
+          }
         ];
       };
     };
